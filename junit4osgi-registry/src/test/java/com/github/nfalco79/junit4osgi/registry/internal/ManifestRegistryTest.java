c8308a7438b521e2aba3a4877049e5011609e17d
Nikolas Falco <nfalco79@hotmail.com>
2017-10-29 14:56:11 +0100

    Fix license Copyright in the header of source files.

--- junit4osgi-registry/src/test/java/com/github/nfalco79/junit4osgi/registry/internal/ManifestRegistryTest.java
+++ junit4osgi-registry/src/test/java/com/github/nfalco79/junit4osgi/registry/internal/ManifestRegistryTest.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2017 Nikolas Falco
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.github.nfalco79.junit4osgi.registry.internal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -24,7 +39,6 @@ import org.mockito.internal.util.io.IOUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.service.log.LogService;
 
-import com.github.nfalco79.junit4osgi.registry.internal.ManifestRegistry;
 import com.github.nfalco79.junit4osgi.registry.internal.util.BundleBuilder;
 import com.github.nfalco79.junit4osgi.registry.spi.TestBean;
 import com.github.nfalco79.junit4osgi.registry.spi.TestRegistryChangeListener;
